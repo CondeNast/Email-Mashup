00393f48712410ff87391fc4debfb620105690aa
JasonDaniel <jason_daniel@condenast.com>
2021-08-10 12:34:40 +0530

    adding env files

--- src/config/qlik-config.js
+++ src/config/qlik-config.js
@@ -1,12 +1,18 @@
-export default {
-  name: "Email Mashup",
-  host: "dash.condenast.com",
-  isSecure: true,
-  port: 443,
-  appname: "3aa806ee-fd80-4b84-b514-5d7080c487a4",
-  //appname: "6b34970a-669d-4083-b1ad-83154555ba4d",
-  initialBookmark: "1c2701de-c7c6-44ee-92aa-5cd4803adb86",
-  initialSelections: [
-    { field: "mailing_status", values: [{ qText: "All Mailings" }] }
-  ]
-};
+export default [
+  {
+    name: process.env.REACT_APP_NAME,
+    host: process.env.REACT_APP_HOST,
+    appname: process.env.REACT_APP_APPNAME,
+    initialBookmark: process.env.REACT_APP_INITIAL_BOOKMARK,
+    isSecure: process.env.REACT_APP_ISSECURE === "true",
+    port: Number(process.env.REACT_APP_PORT),
+    isCloud: process.env.REACT_APP_HOST.includes("qlikcloud") ? true : false,
+    webIntegrationId: process.env.REACT_APP_WEB_INTEGRATION_ID,
+    initialSelections: [
+      {
+        field: process.env.REACT_APP_SELECTION_FIELD,
+        values: [{ qText: process.env.REACT_APP_SELECTION_QTEXT }]
+      }
+    ]
+  }
+];
